7a93cab13bfc50c2adf8b4ecff1792e4829d2e0d
Ashutosh45c <dhokle100@gmail.com>
2024-07-21 23:41:44 +0530

    change title of project

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
   return (
     <div className='py-2 px-6 w-full bg-white text-black border-b-2 flex flex-row items-center justify-between sticky top-0 z-20 ' >
         <div className='flex flex-row items-center justify-center'>
-            <Link to='/'><h1 className='text-xl font-bold m-2 mr-8 ' style={{color:'#FFC100'}}>Amba Nagri </h1></Link>
+            <Link to='/'><h1 className='text-xl font-bold m-2 mr-8 ' style={{color:'#FFC100'}}>PizzeriaExpress </h1></Link>
         </div>
         <div className='flex flex-row items-center justify-center'>  
           <UserMenu/>
