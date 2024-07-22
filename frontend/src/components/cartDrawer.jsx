15220c996b6facf6ca213f335b161df72993122b
Ashutosh Dhokle <166641647+AshuDhokle@users.noreply.github.com>
2024-07-22 13:38:46 +0530

    Update cartDrawer.jsx

--- frontend/src/components/cartDrawer.jsx
+++ frontend/src/components/cartDrawer.jsx
@@ -28,7 +28,7 @@ const CartDrawer = () => {
         address: deliveryAddress
       }
       
-      const response = await axios.post('https//localhost:3000/api/payment/create-checkout-session',body)
+      const response = await axios.post('http//localhost:3000/api/payment/create-checkout-session',body)
       const session = await response.data;
       const result = await stripe.redirectToCheckout({
         sessionId : session.id,
